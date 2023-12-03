96b960d2f88e36c9c208e259808c0db7993e44ed
hà thục anh <hathucanh@hohihehe.local>
2023-12-03 21:55:33 +0700

    check bishop updated

--- chess.py
+++ chess.py
@@ -190,6 +190,41 @@ def check_options(pieces, locations, turn):
         all_moves_list.append(moves_list)
     return all_moves_list
 
+#check bishop moves
+def check_bishop(position, color):
+    moves_list = []
+    if color == 'white':
+        enemies_list = black_locations
+        friends_list = white_locations
+    else:
+        enemies_list = white_locations
+        friends_list = black_locations
+    for i in range(4): # up right, up left, down right, down left
+        path = True
+        chain = 1
+        if i == 0:
+            x = 1
+            y = -1
+        elif i == 1:
+            x = -1
+            y = -1
+        elif i == 2:
+            x = 1
+            y = 1
+        else:
+            x = -1
+            y = 1
+        while path:
+            if (position[0] + (chain * x), position[1] + (chain * y)) not in friends_list and \
+                    0 <= position[0] + (chain * x) <= 7 and 0 <= position[1] + (chain * y) <= 7:
+                moves_list.append((position[0] + (chain * x), position[1] + (chain * y)))
+                if (position[0] + (chain * x), position[1] + (chain * y)) in enemies_list:
+                    path = False
+                chain += 1
+            else:
+                path = False
+    return moves_list
+
 #check rook moves
 def check_rook(position, color):
     moves_list = []
@@ -224,6 +259,7 @@ def check_rook(position, color):
             else:
                 path = False
         return moves_list
+
 
 # main game loop
 black_options = check_options(black_pieces, black_locations, 'black')

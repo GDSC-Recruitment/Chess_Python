a3af8edea80aee26afcbcac168d7fc6ca8f1722a
Tram Anh Nguyen <tramanhxuannguyen0304@gmail.com>
2023-12-03 23:14:02 +0700

    update calculate pawn moves

--- chess.py
+++ chess.py
@@ -154,6 +154,31 @@ def check_king(position, color):
         if target not in friends_list and 0 <= target[0] <= 7 and 0 <= target[1] <= 7:
             moves_list.append(target)
     return moves_list
+
+#Calculate valid pawn moves
+def check_pawn(position, color):
+    moves_list = []
+    if color == 'white':
+        if (position[0], position[1] + 1) not in white_locations and (position[0], position[1] + 1) not in black_locations and position[1] < 7:
+            moves_list.append(position[0], position[1] + 1)
+        if (position[0], position[1] + 2) not in black_locations and (position[0], position[1] + 2) not in black_locations and position[1] == 1:
+            moves_list.append(position[0], position[1] + 2)
+        if (position[0] + 1, position[1] + 1) in black_locations: 
+            moves_list.append(position[0] + 1, position[1] + 1)
+        if (position[0] - 1, position[1] + 1) in black_locations: 
+            moves_list.append(position[0] - 1, position[1] + 1)
+            
+    else:
+        if (position[0], position[1] - 1) not in white_locations and (position[0], position[1] - 1) not in black_locations and position[1] > 0:
+            moves_list.append(position[0], position[1] - 1)
+        if (position[0], position[1] - 2) not in black_locations and (position[0], position[1] - 2) not in black_locations and position[1] == 6:
+            moves_list.append(position[0], position[1] + 2)
+        if (position[0] + 1, position[1] - 1) in white_locations: 
+            moves_list.append(position[0] + 1, position[1] - 1)
+        if (position[0] - 1, position[1] - 1) in white_locations: 
+            moves_list.append(position[0] - 1, position[1] - 1)
+    
+    return moves_list
     
 #Calculate valid queen moves
 def check_queen(position, color):

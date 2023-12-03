c9a9b5f694dad110249494d002b66f5c1504b63d
hà thục anh <hathucanh@hohihehe.local>
2023-12-03 20:22:59 +0700

    updated check_options

--- chess.py
+++ chess.py
@@ -169,8 +169,26 @@ def draw_check():
                         pygame.draw.rect(screen,'dark blue', [black_locations[king_index][0] * 100 + 1,
                                                               black_locations[king_index][1] * 100 + 1, 100, 100], 5)
 #function to check all pieces valid options on board
-def check_options():
-    pass
+def check_options(pieces, locations, turn):
+    moves_list = []
+    all_moves_list = []
+    for i in range((pieces)):
+        location = locations[i]
+        piece = pieces[i]
+        if piece == 'pawn':
+            moves_list = check_pawn(location, turn)
+        elif piece == 'rook':
+            moves_list = check_rook(location, turn)
+        elif piece == 'knight':
+            moves_list = check_knight(location, turn)
+        elif piece == 'bishop':
+            moves_list = check_bishop(location, turn)
+        elif piece == 'queen':
+            moves_list = check_queen(location, turn)
+        elif piece == 'king':
+            moves_list = check_king(location, turn)
+        all_moves_list.append(moves_list)
+    return all_moves_list
 
 # main game loop
 black_options = check_options(black_pieces, black_locations, 'black')

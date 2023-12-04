7103fca795e9c3570b87dc84539c885d3fdf3c94
Vo <hopekasako@gmail.com>
2023-12-04 22:38:58 +0700

    Fix the front-end and check for promotion

--- constants.py
+++ constants.py
@@ -62,9 +62,11 @@ white_pawn = pygame.image.load('assets/images/white pawn.png')
 white_pawn = pygame.transform.scale(white_pawn, (65, 65))
 white_pawn_small = pygame.transform.scale(white_pawn, (45, 45))
 white_images = [white_pawn, white_queen, white_king, white_knight, white_rook, white_bishop]
+white_promotions = ['bishop', 'knight', 'rook', 'queen']
 small_white_images = [white_pawn_small, white_queen_small, white_king_small, white_knight_small,
                       white_rook_small, white_bishop_small]
 black_images = [black_pawn, black_queen, black_king, black_knight, black_rook, black_bishop]
+black_promotions = ['bishop', 'knight', 'rook', 'queen']
 small_black_images = [black_pawn_small, black_queen_small, black_king_small, black_knight_small,
                       black_rook_small, black_bishop_small]
 piece_list = ['pawn', 'queen', 'king', 'knight', 'rook', 'bishop']
